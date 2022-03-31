04bbd9b6358090dcd9aa9fc54afe9d61dd500d0b
RuoyuLiDelancy <98135779+RuoyuLiDelancy@users.noreply.github.com>
2022-03-31 16:29:56 +0100

    Added html js css

--- app.py
+++ app.py
@@ -1,20 +1,21 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 @app.route("/")
 def index():
-
+    
     # Load current count
     f = open("count.txt", "r")
     count = int(f.read())
     f.close()
 
-    # Increment the count
+    # Increment the coun at
     count += 1
 
     # Overwrite the count
-    f = open("count.txt", "w")
+    f = open("../count.txt", "w")
     f.write(str(count))
     f.close()
 
@@ -22,4 +23,4 @@ def index():
     return render_template("index.html", count=count)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
